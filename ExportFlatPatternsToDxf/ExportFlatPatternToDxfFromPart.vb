9cc24ec9536de391d1b4bbc71a1eacde80727953
Eduardo López <edolopez13@gmail.com>
2025-11-11 14:44:02 -0500

    Create ExportFlatPatternToDxfFromPart.vb

---
+++ ExportFlatPatternsToDxf/ExportFlatPatternToDxfFromPart.vb
@@ -0,0 +1,51 @@
+Sub Main ()
+
+    Dim currentDoc As PartDocument = ThisDoc.Document
+	
+	Dim oDlg As New System.Windows.Forms.FolderBrowserDialog
+	Dim dlgResult As New System.Windows.Forms.DialogResult
+	
+    With oDlg
+	
+    .ShowNewFolderButton = True
+    .InitialDirectory = "C:\Temp"
+
+    If .ShowDialog = dlgResult.Cancel
+	    Exit Sub
+	End If
+	
+    End With
+	
+	targetLocation = oDlg.SelectedPath
+	
+            
+            ' Check for sheet metal
+            If currentDoc.ComponentDefinition.Type = kSheetMetalComponentDefinitionObject Then
+
+                Dim smCompDef As SheetMetalComponentDefinition = currentDoc.ComponentDefinition
+                
+                ' verifies if there is flat pattern and creates one if there is not
+                    If Not smCompDef.HasFlatPattern Then
+                        smCompDef.Unfold()
+                    End If
+                
+                ' Gets flat pattern
+                    Dim flatPattern As FlatPattern = smCompDef.FlatPattern
+               
+	            ' Gets name for dxf file
+			        Dim desiredDisplayName As String = currentDoc.DisplayName.Substring(0, currentDoc.DisplayName.LastIndexOf(".")) 'removes ".ipt"
+			    'MsgBox(desiredDisplayName)
+                ' Creates file name
+                    Dim fileName As String = System.IO.Path.Combine(targetLocation, desiredDisplayName & ".dxf") 
+                'MsgBox(fileName & " has been added")
+				
+                ' Export to DXF
+                flatPattern.DataIO.WriteDataToFile("FLAT PATTERN DXF?AcadVersion=2018", fileName)
+            Else
+			    MsgBox("No Existing Flat Pattern within this part")
+				Exit Sub
+				
+            End If
+			
+    MsgBox("Done")
+End Sub
