76343790b009cb1b244355677ba2531ab3c7b2f7
Eduardo López <edolopez13@gmail.com>
2026-02-05 09:37:08 -0500

    Update ImportPropertiesAndMaterialDataFromExcel.vb

--- ExcelIO/ImportPropertiesAndMaterialDataFromExcel/ImportPropertiesAndMaterialDataFromExcel.vb
+++ ExcelIO/ImportPropertiesAndMaterialDataFromExcel/ImportPropertiesAndMaterialDataFromExcel.vb
@@ -43,76 +43,83 @@ Sub setPropertiesAndMat(assembComp As AssemblyDocument, file As String, tab As S
 	Dim listkeyStrings As New List(Of String)
 
     For Each compOcc As ComponentOccurrence In assembComp.ComponentDefinition.Occurrences.AllLeafOccurrences
-		
-	    If compOcc.Suppressed Or TypeOf compOcc.Definition.Document IsNot PartDocument Or compOcc.BOMStructure.Equals(kNormalBOMStructure) <> True Then 
-	        'MsgBox(compOcc.Name)
-	    Else	
-		    Dim occDoc As PartDocument = compOcc.Definition.Document ' Acceso al part document de la instancia
-	        nameToCompare = occDoc.DisplayName.Substring(0, occDoc.DisplayName.LastIndexOf("."))
+	
+		If compOcc.Suppressed Then
+			
+		Else	
+	        If  TypeOf compOcc.Definition.Document IsNot PartDocument Or compOcc.BOMStructure.Equals(kNormalBOMStructure) <> True Then
+	            'MsgBox(compOcc.Name)
+	        Else	
+		        Dim occDoc As PartDocument = compOcc.Definition.Document ' Acceso al part document de la instancia
+	        	nameToCompare = occDoc.DisplayName.Substring(0, occDoc.DisplayName.LastIndexOf("."))
 		   
-		    If listkeyStrings.Contains(nameToCompare) Then
+		    	If listkeyStrings.Contains(nameToCompare) Then
 				
-		    Else
-		        'MsgBox(nameToCheck)
-			     listkeyStrings.Add(nameToCompare)
+		    	Else
+		        	'MsgBox(nameToCheck)
+			     	listkeyStrings.Add(nameToCompare)
 		
-	             For rowCounter = 2 To lastValue
+	             	For rowCounter = 2 To lastValue
 				
-		             Dim nameOccDS As String = GoExcel.CellValue(file, tab, "A" & rowCounter) 
-				     'Dim occDoc As PartDocument = compOcc.Definition.Document ' Acceso al part document de la instancia
-					 Dim occProps As PropertySet = occDoc.PropertySets.Item("Design Tracking Properties")
+		                Dim nameOccDS As String = GoExcel.CellValue(file, tab, "A" & rowCounter) 
+				       'Dim occDoc As PartDocument = compOcc.Definition.Document ' Acceso al part document de la instancia
+					 	Dim occProps As PropertySet = occDoc.PropertySets.Item("Design Tracking Properties")
 				
-					 Dim oCurrentScope As MemberEditScopeEnum
-					 Dim oFactoryDoc As PartDocument
+					 	Dim oCurrentScope As MemberEditScopeEnum
+					 	Dim oFactoryDoc As PartDocument
 				
 				
-				     If occDoc.ComponentDefinition.IsModelStateMember Then
+				    	If occDoc.ComponentDefinition.IsModelStateMember Then
 					
-					     oFactoryDoc = occDoc.ComponentDefinition.FactoryDocument
-					     oCurrentScope = oFactoryDoc.ComponentDefinition.ModelStates.MemberEditScope
+					        oFactoryDoc = occDoc.ComponentDefinition.FactoryDocument
+					     	oCurrentScope = oFactoryDoc.ComponentDefinition.ModelStates.MemberEditScope
 		
-				         If oCurrentScope = MemberEditScopeEnum.kEditActiveMember Then
-					         oFactoryDoc.ComponentDefinition.ModelStates.MemberEditScope = MemberEditScopeEnum.kEditAllMembers
-				    	 End If
-				    	 occDoc = oFactoryDoc
-					 End If			
+				            If oCurrentScope = MemberEditScopeEnum.kEditActiveMember Then
+					            oFactoryDoc.ComponentDefinition.ModelStates.MemberEditScope = MemberEditScopeEnum.kEditAllMembers
+				    	 	End If
+				    	 	occDoc = oFactoryDoc
+					 	End If			
 					 
-					 If occDoc.PropertySets.PropertySetExists("Database Properties") Then
+						If occDoc.PropertySets.PropertySetExists("Database Properties") Then
 						 
-					 Else
-						 occDoc.PropertySets.Add("Database Properties")
-						 occDoc.PropertySets("Database Properties").Add("Part ID")
-					 End If
+					    Else
+						    occDoc.PropertySets.Add("Database Properties")
+						 	occDoc.PropertySets("Database Properties").Add("Part ID")
+					 	End If
 					 
 				
-					'nameToCompare = occDoc.DisplayName.Substring(0, occDoc.DisplayName.LastIndexOf("."))
+						'nameToCompare = occDoc.DisplayName.Substring(0, occDoc.DisplayName.LastIndexOf("."))
 				
 				
-			        'If compOcc.Name = nameOccDS Then
-                 	 If nameToCompare = nameOccDS Then 
-				         'MsgBox(nameToCompare)
-                    	 propsAssigner(nameToCompare & ".ipt", file, tab, rowCounter, colPartNo, colStockNumber, colDescription, colMaterial, colPartID)    'compOcc.Name
+			        	'If compOcc.Name = nameOccDS Then
+                 	 	If nameToCompare = nameOccDS Then 
+				        	'MsgBox(nameToCompare)
+                    	    propsAssigner(nameToCompare & ".ipt", file, tab, rowCounter, colPartNo, colStockNumber, colDescription, colMaterial, colPartID)    'compOcc.Name
 					
-	                	 If oFactoryDoc IsNot Nothing Then
-						     oFactoryDoc.ComponentDefinition.ModelStates.MemberEditScope = oCurrentScope
-						 End If
+	                	If oFactoryDoc IsNot Nothing Then
+					    	oFactoryDoc.ComponentDefinition.ModelStates.MemberEditScope = oCurrentScope
+						End If
 	              
 			         End If
 			     Next 
 		     End If
 	    End If
+	End If
     Next
     			
 End Sub
 
 Sub propsAssigner(compName As String, doc As String, tab As String, row As Integer, partNo As String, stockNumber As String, description As String, material As String, partID As String)
 	
-    iProperties.Expression(compName, "Project", "Part Number") = GoExcel.CellValue(doc, tab, partNo & row)
-	iProperties.Expression(compName, "Project", "Stock Number") = GoExcel.CellValue(doc, tab, stockNumber & row)
-	iProperties.Expression(compName, "Project", "Description") = GoExcel.CellValue(doc, tab, description & row)
-	iProperties.Material(compName) = GoExcel.CellValue(doc, tab, material & row)
-	
-	iProperties.Expression(compName,"Custom","Part ID") = GoExcel.CellValue(doc, tab, partID & row)
+	Try
+        iProperties.Expression(compName, "Project", "Part Number") = GoExcel.CellValue(doc, tab, partNo & row)
+		iProperties.Expression(compName, "Project", "Stock Number") = GoExcel.CellValue(doc, tab, stockNumber & row)
+		iProperties.Expression(compName, "Project", "Description") = GoExcel.CellValue(doc, tab, description & row)
+		iProperties.Material(compName) = GoExcel.CellValue(doc, tab, material & row)
+	Catch
+		MsgBox("Check properties in worksheet for " & compName)
+		
+	End Try
 	
 	
 End Sub
