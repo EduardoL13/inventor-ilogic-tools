061a091f3cfeca8bd1e12758ea03f8bf7f59ff8c
Eduardo López <edolopez13@gmail.com>
2026-02-05 10:33:39 -0500

    Update ExportFlatPatternsToDxfDirect.vb

--- ExportFlatPatternsToDxf/ExportFlatPatternsToDxfDirect.vb
+++ ExportFlatPatternsToDxf/ExportFlatPatternsToDxfDirect.vb
@@ -22,26 +22,28 @@ Sub Main ()
 	Dim listkeyStrings As New List(Of String)
 	
     For Each compOccurrence As ComponentOccurrence In leafOccurrences
+        If compOccurrence.Suppressed Then
 		
-		If compOccurrence.Suppressed Or TypeOf compOccurrence.Definition.Document IsNot PartDocument Then
-        
 		Else
+		    If TypeOf compOccurrence.Definition.Document IsNot PartDocument Then
+        
+		    Else
 
-            Dim occDoc As PartDocument = compOccurrence.Definition.Document
+                Dim occDoc As PartDocument = compOccurrence.Definition.Document
             
-            ' Check for sheet metal
-            If occDoc.ComponentDefinition.Type = kSheetMetalComponentDefinitionObject Then
+                ' Check for sheet metal
+                If occDoc.ComponentDefinition.Type = kSheetMetalComponentDefinitionObject Then
 		    
-			' Check if part is a replica of an existing one    
-		        nameToCheck = compOccurrence.Name.Substring(0, compOccurrence.Name.LastIndexOf(":"))  'trimName(compOccurrence.Name, 2) ' Removes the 2 last chars of the occ name, for example: ":1"
+			        ' Check if part is a replica of an existing one    
+		            nameToCheck = compOccurrence.Name.Substring(0, compOccurrence.Name.LastIndexOf(":"))  'trimName(compOccurrence.Name, 2) ' Removes the 2 last chars of the occ name, for example: ":1"
 				
-		        If listkeyStrings.Contains(nameToCheck) Then
+		            If listkeyStrings.Contains(nameToCheck) Then
 		
-		        Else
-				   'MsgBox(nameToCheck)
-			       listkeyStrings.Add(nameToCheck)
+		            Else
+				       'MsgBox(nameToCheck)
+			           listkeyStrings.Add(nameToCheck)
 		    
-                    Dim smCompDef As SheetMetalComponentDefinition = occDoc.ComponentDefinition
+                       Dim smCompDef As SheetMetalComponentDefinition = occDoc.ComponentDefinition
                 
                 ' verifies if there is flat pattern and creates one if there is not
                     If Not smCompDef.HasFlatPattern Then
@@ -52,15 +54,27 @@ Sub Main ()
                     Dim flatPattern As FlatPattern = smCompDef.FlatPattern
                
 	            ' Gets name for dxf file
-			        Dim desiredDisplayName As String =occDoc.PropertySets.Item("Design Tracking Properties").Item("Stock Number").Value 'Dim desiredDiisplayName.Substring(0, occDoc.DisplayName.LastIndexOf(".")) 'removes ".ipt"
-			    'MsgBox(desiredDisplayName)
+					   Try
+						   
+			               desiredDisplayName = occDoc.PropertySets.Item("Design Tracking Properties").Item("Stock Number").Value
+			           	  'MsgBox(desiredDisplayName)
+					   
+					   Catch
+					       MsgBox("Current part " & nameToCheck & "has no Stock number/Part ID assigned")
+				           Exit For
+					   End Try
 				
                 ' Creates file name
                     Dim fileName As String = System.IO.Path.Combine(targetLocation, desiredDisplayName & ".dxf") 
                 'MsgBox(fileName & " has been added")
 				
                 ' Export to DXF
-                flatPattern.DataIO.WriteDataToFile("FLAT PATTERN DXF?AcadVersion=2018", fileName)
+					   Try
+						 
+                           flatPattern.DataIO.WriteDataToFile("FLAT PATTERN DXF?AcadVersion=2018", fileName)
+                       Catch
+						   MsgBox("Invalid file name for " & nameToCheck & " cut file. Make sure that format is correct and that the name does not contain invalid symbols")
+					   End Try
                 
                 End If
             End If
