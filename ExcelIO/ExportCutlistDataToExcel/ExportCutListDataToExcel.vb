dd035c36ff7e39831f9bf75a94ebcb9f65f80dbf
Eduardo López <edolopez13@gmail.com>
2026-02-05 14:16:26 -0500

    Update ExportCutListDataToExcel.vb

--- ExcelIO/ExportCutlistDataToExcel/ExportCutListDataToExcel.vb
+++ ExcelIO/ExportCutlistDataToExcel/ExportCutListDataToExcel.vb
@@ -50,27 +50,9 @@ For Each compOccurrence As ComponentOccurrence In leafOccurrences
 			Else
 					
 			    Dim partModDims As ModelDimensions = occDoc.ComponentDefinition.ModelAnnotations.ModelDimensions
-				If rowCounter = 8 Then
-	        	    GoExcel.CellValue(file, tab, partIDColumn & rowCounter) = nameToPrint 'confirmar esto
-					GoExcel.CellValue(partQtyColumn & rowCounter) = Single.Parse(occDoc.PropertySets.Item("Design Tracking Properties").Item("Cost Center").Value) 'Total qty
-		
-'					GoExcel.CellValue(partMaterialColumn & rowCounter) = occDoc.PropertySets.Item("Design Tracking Properties").Item("Material").Value
-					GoExcel.CellValue(partDescriptionColumn & rowCounter) = occDoc.PropertySets.Item("Design Tracking Properties").Item("Description").Value
-						
-					GoExcel.CellValue(partLengthColumn & rowCounter) = partModDims.Item("length").ModelValue * ConvFactor
-						
-						
-				Else
-	        		GoExcel.CellValue(file, tab, partIDColumn & rowCounter) = nameToPrint 'confirmar esto
-					GoExcel.CellValue(partQtyColumn & rowCounter) = Single.Parse(occDoc.PropertySets.Item("Design Tracking Properties").Item("Cost Center").Value) 'Total qty
-		
-'					GoExcel.CellValue(partMaterialColumn & rowCounter) = occDoc.PropertySets.Item("Design Tracking Properties").Item("Material").Value
-					GoExcel.CellValue(partDescriptionColumn & rowCounter) = occDoc.PropertySets.Item("Design Tracking Properties").Item("Description").Value
-						
-					GoExcel.CellValue(partLengthColumn & rowCounter) = partModDims.Item("length").ModelValue * ConvFactor
-
-						
-			    End If
+				
+                propsPrinter(occDoc, file, tab, partIDColumn, partQtyColumn, partDescriptionColumn, partLengthColumn, rowStart, rowCounter)
+				
           	    rowCounter = rowCounter + 1						
 
 			End If
@@ -79,4 +61,25 @@ For Each compOccurrence As ComponentOccurrence In leafOccurrences
 Next
 GoExcel.Save
 MsgBox("Export Done")
+End Sub
+
+Sub propsPrinter(currentPart As PartDocument, file As String, tab As String, partIDColumn As String, partQtyColumn As String, partDescriptionColumn As String, partLengthColumn As String, startRow As Integer, currentRow As Integer)
+    Dim ConvFactor As Double = 1 / 2.54 'Factor de conversión de cm a in
+'    If currentRow = startRow Then
+
+		Try
+			
+	        GoExcel.CellValue(file, tab, partIDColumn & currentRow) = currentPart.PropertySets.Item("Design Tracking Properties").Item("Stock Number").Value  'confirmar esto	
+			GoExcel.CellValue(partQtyColumn & currentRow) = Single.Parse(currentPart.PropertySets.Item("Design Tracking Properties").Item("Cost Center").Value) 'Total qty
+			GoExcel.CellValue(partDescriptionColumn & currentRow) = currentPart.PropertySets.Item("Design Tracking Properties").Item("Description").Value
+			GoExcel.CellValue(partLengthColumn & currentRow) = currentPart.ComponentDefinition.ModelAnnotations.ModelDimensions.Item("length").ModelValue * ConvFactor			
+'			GoExcel.CellValue(partMaterialColumn & rowCounter) = occDoc.PropertySets.Item("Design Tracking Properties").Item("Material").Value	
+
+		Catch :
+
+			MsgBox(currentPart.DisplayName & " has missing properties or properties that are not in the valid format")
+		    'GoTo ExitHere 
+		
+		End Try
+
 End Sub
