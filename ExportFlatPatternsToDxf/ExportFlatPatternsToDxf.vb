c38ed423694bb8832ab0e5f6ddc8c361d2def879
Eduardo López <edolopez13@gmail.com>
2026-02-09 15:41:56 -0500

    Update ExportFlatPatternsToDxf.vb

--- ExportFlatPatternsToDxf/ExportFlatPatternsToDxf.vb
+++ ExportFlatPatternsToDxf/ExportFlatPatternsToDxf.vb
@@ -2,87 +2,84 @@ Sub Main ()
 
     Dim currentDoc As AssemblyDocument = ThisDoc.Document
     Dim leafOccurrences As ComponentOccurrencesEnumerator = currentDoc.ComponentDefinition.Occurrences.AllLeafOccurrences
-
-    ' This macro browse lets you browse the location and add it as an invisible custom property.
-
-    projectVba = "ApplicationProject" ' Project's name where the macro will be ran
-    moduleVba = "ModFileBrowser" 'Module within the project that contains the macro
-    macroVba = "BrowseLocation" 'Name of the macro to run
-
-	InventorVb.RunMacro(projectVba, moduleVba, macroVba)
-
-    ' Access the property set created int the macro ran previously
-    Dim dxfData As PropertySet = currentDoc.PropertySets.Item("dxf Export Data")
-	targetLocation = dxfData.Item("Location").Value
 	
-    'Input target location by copy and paste 
+	Dim oDlg As New System.Windows.Forms.FolderBrowserDialog
+	Dim dlgResult As New System.Windows.Forms.DialogResult
 	
-    'targetLocation = InputBox("Copy and paste the path where you want the dxf files to be created","Location for dxf")
-	'targetLocation = targetLocation & "\"
+    With oDlg
+	
+    .ShowNewFolderButton = True
+    .InitialDirectory = "C:\Temp"
+
+    If .ShowDialog = dlgResult.Cancel
+	    Exit Sub
+	End If
+	
+    End With
+	
+	targetLocation = oDlg.SelectedPath
 	
 	Dim listkeyStrings As New List(Of String)
 	
     For Each compOccurrence As ComponentOccurrence In leafOccurrences
+        If compOccurrence.Suppressed Then
 		
-		If compOccurrence.Suppressed Or TypeOf compOccurrence.Definition.Document IsNot PartDocument Then
-        
 		Else
-			
-	        'MsgBox(compOccurrence.Name)
-			
-			
-            Dim occDoc As PartDocument = compOccurrence.Definition.Document
+		    If TypeOf compOccurrence.Definition.Document IsNot PartDocument Then
+        
+		    Else
+
+                Dim occDoc As PartDocument = compOccurrence.Definition.Document
             
-            ' Check for sheet metal
-            If occDoc.ComponentDefinition.Type = kSheetMetalComponentDefinitionObject Then
+                ' Check for sheet metal
+                If occDoc.ComponentDefinition.Type = kSheetMetalComponentDefinitionObject Then
 		    
-			' Check if part is a replica of an existing one    
-		        nameToCheck = trimName(compOccurrence.Name, 2) ' Removes the 2 last chars of the occ name, for example: ":1"
-		        If listkeyStrings.Contains(nameToCheck) Then
+			        ' Check if part is a replica of an existing one    
+		            nameToCheck = compOccurrence.Name.Substring(0, compOccurrence.Name.LastIndexOf(":"))  'trimName(compOccurrence.Name, 2) ' Removes the 2 last chars of the occ name, for example: ":1"
+				
+		            If listkeyStrings.Contains(nameToCheck) Then
 		
-		        Else
-			       listkeyStrings.Add(nameToCheck)
+		            Else
+				       'MsgBox(nameToCheck)
+			           listkeyStrings.Add(nameToCheck)
 		    
-                Dim smCompDef As SheetMetalComponentDefinition = occDoc.ComponentDefinition
+                       Dim smCompDef As SheetMetalComponentDefinition = occDoc.ComponentDefinition
                 
                 ' verifies if there is flat pattern and creates one if there is not
-                If Not smCompDef.HasFlatPattern Then
-                    smCompDef.Unfold()
-                End If
+                    If Not smCompDef.HasFlatPattern Then
+                        smCompDef.Unfold()
+                    End If
                 
                 ' Gets flat pattern
-                Dim flatPattern As FlatPattern = smCompDef.FlatPattern
-                
+                    Dim flatPattern As FlatPattern = smCompDef.FlatPattern
+               
 	            ' Gets name for dxf file
-			    Dim desiredDisplayName As String = trimName(occDoc.DisplayName,4) 'removes 4 chars: ".ipt"
-			    'MsgBox(desiredDisplayName)
+					   Try
+						   
+			               desiredDisplayName = occDoc.PropertySets.Item("Design Tracking Properties").Item("Stock Number").Value
+			           	  'MsgBox(desiredDisplayName)
+					   
+					   Catch
+					       MsgBox("Current part " & nameToCheck & "has no Stock number/Part ID assigned")
+				           Exit For
+					   End Try
 				
                 ' Creates file name
-                Dim fileName As String = System.IO.Path.Combine(targetLocation, desiredDisplayName & ".dxf") 
+                    Dim fileName As String = System.IO.Path.Combine(targetLocation, desiredDisplayName & ".dxf") 
                 'MsgBox(fileName & " has been added")
 				
                 ' Export to DXF
-                flatPattern.DataIO.WriteDataToFile("FLAT PATTERN DXF?AcadVersion=2018", fileName)
+					   Try
+						 
+                           flatPattern.DataIO.WriteDataToFile("FLAT PATTERN DXF?AcadVersion=2018", fileName)
+                       Catch
+						   MsgBox("Invalid file name for " & nameToCheck & " cut file. Make sure that format is correct and that the name does not contain invalid symbols")
+					   End Try
                 
+                    End If
                 End If
             End If
-        End If
-        Next
+	    End If 
+    Next
     MsgBox("All dxf files have been added to the chosen location")
 End Sub
-
-
-Function trimName (nameToTrim As String, noChar As Integer )
-
-   Dim limSup As Integer = Len(nameToTrim) 
-   Dim limInf As Integer = limSup - noChar
-   Dim trimmedString As String = nameToTrim
-   
-   trimmedString = trimmedString.Remove(limInf)
-   Return trimmedString
-   
-End Function
-
-
-
-
